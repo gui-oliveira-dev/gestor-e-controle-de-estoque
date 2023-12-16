78f76baa4359f638775303f5bae5e5081ff49a91
Guilherme Oliveira <guigooliveraa@gmail.com>
2023-12-16 11:10:29 -0300

    faz uma estilização básica na pagina home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,30 @@
 import { Link } from 'react-router-dom';
+import styles from './home.module.css';
+import AddImage from '../../assets/AddImage.png';
 
 function Home() {
   return (
-    <>
-      <Link to="/storage/new-item"><button>Adicionar produtos ao estoque</button></Link>
-      <Link to="/services/new-service"><button>Adicionar Novo serviço</button></Link>
-      <Link to="/storage"><button>Ver Produtos em estoque</button></Link>
-      <Link to="/services"><button>Ver Serviços em andamento</button></Link>
-    </>
+    <div className={ styles.container }>
+      <Link className={ styles.buttoncard } to="/storage/new-item">
+        <img className={ styles.addimage } src={ AddImage } alt="addImage" />
+        <h2 className={ styles.titlebutton }>Adicionar produtos ao estoque</h2>
+      </Link>
+
+      <Link className={ styles.buttoncard } to="/services/new-service">
+        <img className={ styles.addimage } src={ AddImage } alt="addImage" />
+        <h2 className={ styles.titlebutton }>Adicionar Novo serviço</h2>
+      </Link>
+
+      <Link className={ styles.buttoncard } to="/storage">
+        <img className={ styles.addimage } src={ AddImage } alt="addImage" />
+        <h2 className={ styles.titlebutton }>Ver Produtos em estoque</h2>
+      </Link>
+
+      <Link className={ styles.buttoncard } to="/services">
+        <img className={ styles.addimage } src={ AddImage } alt="addImage" />
+        <h2 className={ styles.titlebutton }>Ver Serviços em andamento</h2>
+      </Link>
+    </div>
   );
 }
 
